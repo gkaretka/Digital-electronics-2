e744f3b06bae2b02e2cad545c707956159573b03
Gregor Karetka <karetka.gregor@gmail.com>
2021-09-23 11:47:53 +0200

    With PWM output for buzzer

--- Labs/01-tools/led/led/main.c
+++ Labs/01-tools/led/led/main.c
@@ -51,7 +51,7 @@ char *morse_numbers[10] = {
     "-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."
 };
 
-#define ONE_SPACE		250
+#define ONE_SPACE		500
 
 #define DOT '.'
 #define DOT_DELAY		ONE_SPACE
@@ -80,16 +80,38 @@ int main(void)
     // PORTB = PORTB and 1101 1111
     PORTB = PORTB & ~(1<<LED_GREEN);
 
+    // PB1 as input (for now)
+    DDRD &= ~(1 << DDD3);
+    // set PWM for 50% duty cycle at 10bit
+    OCR1A = 0x01FF;
+    TCCR1A |= (1 << COM1A1);
+    // set non-inverting mode
+    TCCR1A |= (1 << WGM11) | (1 << WGM10);
+    // set 10bit phase corrected PWM Mode
+    TCCR1B |= (1 << CS11);
+
     // Infinite loop
     while (1)
     {
-        dispaly_message_morse_code("DE2/");
+        /* Char '/' is used for sending additional spaces
+         * You can try any combination
+         * Tested with: 
+         * BPC/DE2/
+         * DE2/
+         * de2/
+         */
+        
+        char *msg = "DE2/";
+        dispaly_message_morse_code(msg);
     }
 
     // Will never reach this
     return 0;
 }
 
+/*
+ * Takes *char(string) as input and separates it into individual characters.
+ */
 void dispaly_message_morse_code(char *msg)
 {
     char *msg_ptr = msg;
@@ -98,52 +120,75 @@ void dispaly_message_morse_code(char *msg)
             send_space(); 
         } else {
             display_char_in_morse_code(*msg_ptr);
+            send_space(); 
         }                
         msg_ptr++;
     }
 }
 
+/*
+ * Look-up table for characters in Morse code alphabet/numbers. 
+ */
 void display_char_in_morse_code(char c)
 {
-    if (c >= 65 && c <= 95) {
+    if (c >= 65 && c <= 95) { // if char is ASCII A-Z
         ll_display_char_in_morse_code(morse_alphabet[(uint8_t)c - 65]);
-    } else if (c >= 97 && c <= 122) {
+    } else if (c >= 97 && c <= 122) { // if char is ASCII a-z
         ll_display_char_in_morse_code(morse_alphabet[(uint8_t)c - 97]);
-    } else if (c >= 48 && c <= 57) {
+    } else if (c >= 48 && c <= 57) { // if char is ASCII 0-9
         ll_display_char_in_morse_code(morse_numbers[(uint8_t)c - 48]);
     }
 }
 
+/*
+ * Low level function for Morse code display, check one by one char and 
+ * performs action accordingly by sending COMMA or DOT
+ */
 void ll_display_char_in_morse_code(char *char_codes)
 {
     char *msg_ptr = char_codes;
     while(*msg_ptr != '\0') {
-        if (*msg_ptr == COMMA)
-            send_comma();
-        else if (*msg_ptr == DOT)
-            send_dot();
+        if (*msg_ptr == COMMA) send_comma();
+        else if (*msg_ptr == DOT) send_dot();
+
         msg_ptr++;
     }        
 }
 
+/*
+ * Low level send space by manipulating pin and delaying
+ */
 void send_space(void)
 {
     PORTB &= ~(1 << LED_GREEN);
+    DDRD &= ~(1 << DDD3);
     _delay_ms(SPACE_DELAY);
 }
 
+/*
+ * Low level send comma by manipulating pin and delaying
+ */
 void send_comma(void)
 {
     PORTB |= (1 << LED_GREEN);
+    DDRD |= (1 << DDD3);
     _delay_ms(COMMA_DELAY);
+    
     PORTB &= ~(1 << LED_GREEN);
+    DDRD &= ~(1 << DDD3);
     _delay_ms(ONE_SPACE);
 }
 
+/*
+ * Low level send dot by manipulating pin and delaying
+ */
 void send_dot(void)
 {
     PORTB |= (1 << LED_GREEN);
+    DDRD |= (1 << DDD3);
     _delay_ms(DOT_DELAY);
+    
     PORTB &= ~(1 << LED_GREEN);
+    DDRD &= ~(1 << DDD3);
     _delay_ms(ONE_SPACE);
 }
