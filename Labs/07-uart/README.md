d156d0c20340bc16abcfaf542eb939c10b6238e8
Gregor Karetka <karetka.gregor@gmail.com>
2021-11-03 12:50:43 +0100

    add lab7

--- Labs/07-uart/README.md
+++ Labs/07-uart/README.md
@@ -28,3 +28,25 @@ Link to your `Digital-electronics-2` GitHub repository:
    | Left   | 1.970&nbsp;V | 403 | todo |
    | Select | 3.182&nbsp;V | 651 | todo |
    | none   | 5&nbsp;V | 1023 | todo |
+
+The operation with the AD converter is performed through ADMUX, ADCSRA, ADCL+ADCH, ADCSRB, and DIDR0 registers. See [ATmega328P datasheet](https://www.microchip.com/wwwproducts/en/ATmega328p) (**Analog-to-Digital Converter > Register Description**) and complete the following table.
+
+   | **Operation** | **Register(s)** | **Bit(s)** | **Description** |
+   | :-- | :-: | :-: | :-- |
+   | Voltage reference    | ADMUX | REFS1:0 | 01: AVcc voltage reference (5V), ... |
+   | Input channel        | ADMUX | MUX3:0 | 0000: ADC0, 0001: ADC1, ... |
+   | ADC enable           | ADCSRA | ADEN | 1: ADC Enable, 0: ADC Disabled |
+   | Start conversion     | ADCSRA | ADSC | 1: Start first conversion/start conversion, 0: stop conversion |
+   | ADC interrupt enable | ADCSRA | ADIE | 1: Enable interrupt, 0: Disable interrupt |
+   | ADC clock prescaler  | ADCSRA | ADPS2:0 | 000: Division factor 2, 001: 2, 010: 4, ...|
+   | ADC 10-bit result    | ADMUX | ADLAR | 0: 10-bit mode(ADCH:L), 1: 8-bit mode(values stored only in ADCH) |
+
+In the lab, we are using [UART library](http://www.peterfleury.epizy.com/avr-software.html) developed by Peter Fleury. Use online manual of UART library and add the input parameters and description of the functions to the following table.
+
+
+   | **Function name** | **Function parameter(s)** | **Description** | **Example** |
+   | :-- | :-- | :-- | :-- |
+   | `uart_init` | `UART_BAUD_SELECT(9600, F_CPU)` | Initialize UART to 8N1 and set baudrate to 9600&nbsp;Bd | `uart_init(UART_BAUD_SELECT(9600, F_CPU));` |
+   | `uart_getc` | `void` | Get received byte from ringbuffer. | `uint8_t c = uart_getc();`
+   | `uart_putc` | `unsigned char data` | Put byte to ringbuffer for transmitting via UART. | `uart_putc('c');`
+   | `uart_puts` | `const char *s` | Put string to ringbuffer for transmitting via UART. | `uart_puts("msg");`
---
+++ Labs/07-uart/lab_7/lab_7/main.c
@@ -0,0 +1,172 @@
+/***********************************************************************
+ * 
+ * Analog-to-digital conversion with displaying result on LCD and 
+ * transmitting via UART.
+ * ATmega328P (Arduino Uno), 16 MHz, AVR 8-bit Toolchain 3.6.2
+ *
+ * Copyright (c) 2018-Present Tomas Fryza
+ * Dept. of Radio Electronics, Brno University of Technology, Czechia
+ * This work is licensed under the terms of the MIT license.
+ * 
+ **********************************************************************/
+
+#ifndef F_CPU
+#define F_CPU 16000000L
+#endif
+
+/* Includes ----------------------------------------------------------*/
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
+#include "timer.h"          // Timer library for AVR-GCC
+#include "lcd.h"            // Peter Fleury's LCD library
+#include <stdlib.h>         // C library. Needed for conversion function
+#include "uart.h"           // Peter Fleury's UART library
+
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Use Timer/Counter1 and start ADC conversion four times 
+ *           per second. Send value to LCD and UART.
+ * Returns:  none
+ **********************************************************************/
+int main(void)
+{
+    // Initialize LCD display
+    lcd_init(LCD_DISP_ON);
+    lcd_gotoxy(1, 0); lcd_puts("value:");
+    lcd_gotoxy(3, 1); lcd_puts("key:");
+    lcd_gotoxy(8, 0); lcd_puts("a");    // Put ADC value in decimal
+    lcd_gotoxy(13,0); lcd_puts("b");    // Put ADC value in hexadecimal
+    lcd_gotoxy(8, 1); lcd_puts("c");    // Put button name here
+
+    // Configure ADC to convert PC0[A0] analog value
+    // Set ADC reference to AVcc
+    ADMUX |= (1 << REFS0);
+    ADMUX &= ~(1 << REFS1);
+    
+    // Set input channel to ADC0
+    ADMUX &= ~(0x07 << MUX0);
+    
+    // Enable ADC module
+    ADCSRA |= (1 << ADEN);
+    
+    // Enable conversion complete interrupt
+    ADCSRA |= (1 << ADIE);
+    
+    // Set clock prescaler to 128
+    ADCSRA |= (1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2);
+
+    // Configure 16-bit Timer/Counter1 to start ADC conversion
+    // Set prescaler to 262 ms and enable overflow interrupt
+    ADCSRB |= (1 << ADTS2) | (1 << ADTS1);
+    ADCSRB &= ~(1 << ADTS0);
+    
+    // Timer 1 set and enable
+    TIM1_overflow_262ms();
+    TIM1_overflow_interrupt_enable();
+    
+    // ADC Start Conversion
+    ADCSRA |= (1 << ADSC);
+    
+    // ADC Auto Trigger Enable
+    ADCSRA |= (1 << ADATE);
+    
+    // Set 10-bit mode
+    ADMUX &= ~(1 << ADLAR);
+    
+    // Initialize UART to asynchronous, 8N1, 9600
+    uart_init(UART_BAUD_SELECT(9600, F_CPU));
+    
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+
+    // Infinite loop
+    while (1)
+    {
+        /* Empty loop. All subsequent operations are performed exclusively 
+         * inside interrupt service routines ISRs */
+    }
+
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Use single conversion mode and start conversion four times
+ *           per second.
+ **********************************************************************/
+ISR(TIMER1_OVF_vect)
+{
+    // not neeeded
+}
+
+/**********************************************************************
+ * Function: ADC complete interrupt
+ * Purpose:  Display value on LCD and send it to UART.
+ **********************************************************************/
+ISR(ADC_vect)
+{
+    uint16_t value = 0;
+    char lcd_string[4] = "0000";
+    value = ADC;                  // Copy ADC result to 16-bit variable
+    
+    // Clear display and convert to deca (and show)
+    lcd_gotoxy(8, 0);
+    lcd_puts("    ");
+    itoa(value, lcd_string, 10);  // Convert decimal value to string
+    lcd_gotoxy(8, 0);
+    lcd_puts(lcd_string);
+    
+    // UART: " value: xxxx "
+    uart_puts("\033[4;32m");
+    uart_puts(" value: ");
+    uart_puts(lcd_string);
+    uart_puts(" ");
+    
+    // Clear display and display hexa value
+    lcd_gotoxy(13, 0);
+    lcd_puts("    ");
+    itoa(value, lcd_string, 16);  // Convert decimal value to string
+    lcd_gotoxy(13, 0);
+    lcd_puts(lcd_string);
+    
+    // hexa string + newline
+    uart_puts(lcd_string);
+    uart_puts("\r\n   key: ");
+    
+    // clear display
+    lcd_gotoxy(8, 1);
+    lcd_puts("      ");
+    
+    // Display on LCD and serial 
+    if (value < 75) {
+        lcd_gotoxy(8, 1);
+        lcd_puts("Right");
+        uart_puts("Right");
+    } else if (value < 150) {
+        lcd_gotoxy(8, 1);
+        lcd_puts("Up");
+        uart_puts("Up");
+    } else if (value < 350) {
+        lcd_gotoxy(8, 1);
+        lcd_puts("Down");
+        uart_puts("Down");
+    } else if (value < 550) {
+        lcd_gotoxy(8, 1);
+        lcd_puts("Left");
+        uart_puts("Left");
+    } else if (value < 800) {
+        lcd_gotoxy(8, 1);
+        lcd_puts("Select");
+        uart_puts("Select");
+    } else {
+        lcd_gotoxy(8, 1);
+        lcd_puts("None");
+        uart_puts("None");
+    }
+    
+    // UART 2xnewlines
+    uart_puts("\r\n\r\n");
+}
